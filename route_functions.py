98804d0f47aa7f38eebd6a2832ff74491986cdfe
Akshay Singh <singhak@uw.edu>
2016-06-27 05:14:50 -0700

    Separate concern

--- route_functions.py
+++ route_functions.py
@@ -1,13 +1,10 @@
 from flask import (
-Flask,
 request,
 render_template
 )
 import json
 from get_govdata import (get_gov_data,format_JSON_data)
-
-app=Flask(__name__,static_url_path='')
-app.config['DEBUG'] = True
+from __init__ import app
 
 @app.route('/')
 def index():
@@ -19,9 +16,6 @@ def return_JSON():
 	valid_response=format_JSON_data(response)
 	return json.dumps(valid_response).encode('utf8')
 	
-"""Running an instance of the flask application"""
-if __name__ == '__main__':
-	app.run()
 
 	
 	
---
+++ run.py
@@ -0,0 +1,3 @@
+from __init__ import app
+"""Running an instance of the flask application"""
+app.run(debug=True)
